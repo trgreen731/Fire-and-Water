7421955718253de2bdce107c36e03605315641b5
trgreen731 <trgreen731@gmail.com>
2019-04-03 02:15:36 -0500

    cleaning up the xml size and usage

--- src/simplestategame/GamePlaySaves.java
+++ src/simplestategame/GamePlaySaves.java
@@ -17,12 +17,10 @@ public class GamePlaySaves {
 			DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
 			DocumentBuilder builder = factory.newDocumentBuilder();
 			Document doc = builder.parse(new File("lib/Saves.xml"));
-			Element saves = doc.getDocumentElement();
 			
-			NodeList completed = saves.getElementsByTagName("completed");
-			Element comp = (Element) completed.item(0);
-			String comp1 = comp.getElementsByTagName("one").item(0).getTextContent();
-			String comp2 = comp.getElementsByTagName("two").item(0).getTextContent();
+			Element saves = doc.getDocumentElement();
+			String comp1 = saves.getElementsByTagName("one").item(0).getTextContent();
+			String comp2 = saves.getElementsByTagName("two").item(0).getTextContent();
 			this.comp_level.add(comp1);
 			this.comp_level.add(comp2);
 			
@@ -46,10 +44,10 @@ public class GamePlaySaves {
 			DocumentBuilderFactory factory_v2 = DocumentBuilderFactory.newInstance();
 			DocumentBuilder builder_v2 = factory_v2.newDocumentBuilder();
 			Document doc_v2 = builder_v2.parse(new File("lib/Saves.xml"));
+			
 			Element saves_v2 = doc_v2.getDocumentElement();
-			NodeList completed_v2 = saves_v2.getElementsByTagName("completed");
-			Element comp_v2 = (Element) completed_v2.item(0);
-			comp_v2.getElementsByTagName(level).item(0).setTextContent(status);
+			saves_v2.getElementsByTagName(level).item(0).setTextContent(status);
+			
 			TransformerFactory transformerfactory = TransformerFactory.newInstance();
 			Transformer transformer = transformerfactory.newTransformer();
 			DOMSource source = new DOMSource(doc_v2);
